9ff4138399a848fb986d6d1fc7de886abfb0ced9
prathamprasain31 <prathamprasain31@gmail.com>
2026-04-24 18:11:34 +0545

    Hollow rectangle

---
+++ task29.cpp
@@ -0,0 +1,17 @@
+/*Hollow rectangle pattern printing*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int r = 5, c = 10;
+
+    for(int i = 1; i <= r; i++) {
+        for(int j = 1; j <= c; j++) {
+            if(i == 1 || i == r || j == 1 || j == c)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
