aa888e70a3a823643289d30ae3a782e354706e90
prathamprasain31 <prathamprasain31@gmail.com>
2026-04-25 21:56:34 +0545

    Print 1 to 10 Using for loops

---
+++ task30.cpp
@@ -0,0 +1,10 @@
+// print the number 1 to 10 using for loops
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
