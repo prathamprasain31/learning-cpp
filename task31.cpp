afbbef1353f0b6a60e35b049488388595612f4d5
prathamprasain31 <prathamprasain31@gmail.com>
2026-04-25 22:01:24 +0545

    Sum of First N numbers

---
+++ task31.cpp
@@ -0,0 +1,16 @@
+// sum of first N numbers using for loops
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
