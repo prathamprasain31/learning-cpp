a4a3044deda229fb037ade6694b1392931fb66cc
prathamprasain31 <prathamprasain31@gmail.com>
2026-04-25 22:06:54 +0545

    Reverse of any numbers using while

---
+++ task34.cpp
@@ -0,0 +1,18 @@
+//program to find the reverse of any numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0;
+    cout << "Enter number: ";
+    cin >> n;
+
+    while(n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    cout << "Reversed number = " << rev;
+    return 0;
+}
