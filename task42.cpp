5f9bffcdf133dbb08aeb08b848d942c2c434b9a2
prathamprasain31 <prathamprasain31@gmail.com>
2026-04-27 23:33:05 +0545

    Sorted number in descending order

---
+++ task42.cpp
@@ -0,0 +1,36 @@
+// sorted number in descending order
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100]; 
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Bubble Sort Logic
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] < arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array in Descending order:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
