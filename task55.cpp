d82191eca05a91d9c0c946f9879937b88f6bdccf
prathamprasain31 <prathamprasain31@gmail.com>
2026-05-11 22:39:59 +0545

    concatenate two words

---
+++ task55.cpp
@@ -0,0 +1,10 @@
+// concatenation of two words
+#include<iostream>
+using namespace std;
+int main(){
+  string a = "Hello";
+  string b ="coders";
+  string c = a + " " +b;
+  cout<<c<<endl;
+    return 0;
+}
